d50cd503719ab4820fd99288392d5e160a6e4159
victordiec <victor.diec@me.com>
2016-03-24 00:01:28 -0400

    If a undefined variable is found in an assignment expression or boolean expression, throw an exception which kills compilation.

---
+++ src/UndefinedVariableException.java
@@ -0,0 +1,13 @@
+public class UndefinedVariableException extends Exception {
+	
+	public UndefinedVariableException()
+	{
+		super();
+	}
+	
+	public UndefinedVariableException(String message)
+	{
+		super(message);
+	}
+	
+}
